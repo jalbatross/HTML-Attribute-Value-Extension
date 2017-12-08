e1a87ae1a1031dda91774a41f8ec6a2d05bd8a15
jalbatross <ih8u2o@gmail.com>
2017-12-07 18:59:36 -0800

    Function in popup.js

--- popup.js
+++ popup.js
@@ -1,5 +1,27 @@
 chrome.browserAction.onClicked.addListener(function(tab) {
-	var execution = 'console.log($("id=[problemId]"));'
-	
-   chrome.tabs.executeScript(null, execution);
+   chrome.tabs.executeScript(null, {
+      file: "jquery-3.2.1.min.js"
+   }, function() {
+      chrome.tabs.executeScript(null, {
+         file: "execute.js"
+      });
+   });
 });
+
+/** 
+ * Search the current page for an HTML element that has an attribute.
+ * 
+ * @param  {string} attribute An HTML element attribute.
+ * @return Empty array if no element or multiple elements were found with the 
+ *         attribute, otherwise an array with the first element as the attribute
+ *         value
+ */
+function findHtmlAttribute(attribute) {
+
+	var script = $("div[" + attribute + "]").attr(attribute);
+
+	chrome.tabs.executeScript(null, {file: "jquery-3.2.1.min.js"}, function() {
+		 return chrome.tabs.executeScript({code: script});
+	});
+}
+
